e0a0434b9f21d0a7e23613af4a0dc361d3ecf84d
JoaoPFranca <jpbarboza2004@gmail.com>
2025-07-18 16:29:55 -0300

    feat: Adiciona arquivos do Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "ID do projeto no Google Cloud Platform"
+  type        = string
+  default     = "ufrn-devops-2025-01"
+}
+
+variable "region" {
+  description = "A região do deploy"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "Cluster do Google Kubernetes Engine"
+  type        = string
+  default     = "devops-cluster-1"
+}
+
+variable "gcp_credentials" {
+  description = "Credenciais para acesso ao GCP"
+  type = string
+}
